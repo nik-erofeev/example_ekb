9da74028006d70db4a8b8aa9290cc22fd8c1a8c4
Erofeev Nikita <erofeev.nik.it@yandex.ru>
2024-02-12 18:43:55 +0300

    12313

--- app/tests/integration_tests/test_shift_tasks/test_api.py
+++ app/tests/integration_tests/test_shift_tasks/test_api.py
@@ -54,3 +54,24 @@ async def test_create_shift_tasks(ac: AsyncClient):
     assert response.status_code == 201
 
 
+
+
+async def test_create_unique_codes(ac: AsyncClient):
+
+    response = await ac.post(
+        "/api/v1/codes/",
+        json=[
+            {
+                "УникальныйКодПродукта": "12gRV60MMsn1",
+                "НомерПартии": 22222,
+                "ДатаПартии": "2024-01-30",
+            },
+            {
+                "УникальныйКодПродукта": "12gRV60MMsn2",
+                "НомерПартии": 22223,
+                "ДатаПартии": "2024-03-30",
+            },
+        ],
+    )
+
+    assert response.status_code == 201
