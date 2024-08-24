4e15a71c720994d3d4572afecb1f986a79fef98d
Erofeev Nikita <erofeev.nik.it@yandex.ru>
2024-08-24 15:46:18 +0300

    fix .sh

--- docker/app.sh
+++ docker/app.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Starting migrations..."
 alembic upgrade head
+MIGRATION_STATUS=$?
+if [ $MIGRATION_STATUS -ne 0 ]; then
+  echo "Migrations failed with status $MIGRATION_STATUS"
+  exit 1
+fi
 
+echo "Starting Uvicorn..."
 uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
